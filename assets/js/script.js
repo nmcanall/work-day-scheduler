b8d69be6778719d9c2cbb3fd377829a151a94d8a
nmcanall <nmcanall@citadel.edu>
2020-06-21 23:17:06 -0700

    save btn uses sibling

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,18 +27,17 @@ var createTimeBlock = function(hour, task) {
 // Load schedule
 var loadSchedule = function() {
     var startTime = moment().hour(9);
-    var endTime = moment().hour(17);
+    var endTime = moment().hour(18);
     for(var time = startTime; time.isBefore(endTime); time.add(1, "hour")) {
-        createTimeBlock(time, "event description");
+        createTimeBlock(time, "");
     }
 }
 
 // Helper method to set the background color based on current time
 var setBackground = function(hour, descriptionEl) {
     // Determine past, present, or future and set background accordingly
-    var currentHour = parseInt(moment().hour(11).format("H"));
+    var currentHour = parseInt(moment().hour());
     var displayHour = parseInt(hour.format("H"));
-    console.log(currentHour, displayHour);
     if(displayHour < currentHour) {
         descriptionEl.addClass("past");
     }
@@ -50,39 +49,40 @@ var setBackground = function(hour, descriptionEl) {
     }
 }
 
-// Helper method to get last class name (used to find current time status)
-var getClassName = function(element) {
-    classArray = element.className.split(" ");
-    return classArray[classArray.length - 1];
-};
 
 // Edit task description by clicking on it
 $("#list").on("click", ".description", function() {
-    var className = getClassName(this);
+    // var className = getClassName(this);
     var text = $(this).text().trim();
-    var textInput = $("<textarea>").addClass("form-control col-10 description" + className).val(text);
+    var textInput = $("<textarea>").addClass("form-control col-10 description").val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
-
-    // Save task when save button is clicked
-    $("#list").on("click", "span", function() {
-        var eventP = $("<p>").addClass("col-10 description " + className).text(textInput[0].value.trim());
-        $(textInput).replaceWith(eventP);
-    });
 });
 
-// $("#list").on("click", "span", function() {
-//     console.log($($(this).siblings(".description")));
-// });
-
-// Close form group
-// $("#list").on("blur", ".description", function() {
-//     console.log($(this).closest(".description"));
-// });
-
 // Save task when save button is clicked
-// $("#list").on("click", "span", function() {
-// });
+$("#list").on("click", "span", function() {
+    var textInputEl = $(this).siblings(".description");
+
+    // If textInputEl is a p element, that means we should not process the save
+    if(textInputEl.is("p")) {
+        return;
+    }
+
+    // Determine current hour and convert to moment format
+    var hour = $(this).siblings(".hour").text();
+    if(hour.slice(-2) === "AM") {
+        hour = parseInt(hour.slice(0, -2));
+    }
+    else {
+        hour = parseInt(hour.slice(0, -2)) + 12;
+    }
+    hour = moment().set("hour", hour);
+
+    // Replace the textInputEl with the p tag
+    var eventP = $("<p>").addClass("col-10 description").text(textInputEl.val().trim());
+    setBackground(hour, eventP);
+    textInputEl.replaceWith(eventP);
+});
 
 // On start-up, set date at the top
 setCurrentDay();
